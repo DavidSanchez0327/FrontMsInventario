82ea07fd0ce0cf233e3b3a2985999f376074fc25
David Mongui <cristhian.mongui@sofka.com.co>
2019-09-10 23:06:32 -0500

    inventario

--- src/App.js
+++ src/App.js
@@ -1,26 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+    deletePost(ev) {
+        let el = ev.target
+        let id = el.dataset.id
+        let index = el.dataset.index
+
+        fetch(`http://localhost:8000/medicamentos/eliminar/:codigo`, {
+            method: 'DELETE'
+        })
+            .catch(err => console.error(err))
+            .then(() => {
+                let posts = this.state.posts
+                posts.splice(index, 1)
+                this.setState({ posts })
+            })
+    }
+
+
+
+
+    constructor(props){
+        super(props);
+        this.state={
+            title: 'Inventario',
+            act: 0,
+            index: '',
+            datas: []
+        }
+    }
+
+    componentDidMount(){
+
+        this.refs.name.focus();
+    }
+
+
+
+    fSubmit = (e) =>{
+        e.preventDefault();
+        console.log('try');
+
+        let datas = this.state.datas;
+        let name = this.refs.name.value;
+        let code = this.refs.code.value;
+        let pricePurchase = this.refs.pricePurchase.value;
+        let salePrice = this.refs.salePrice.value;
+        let stock = this.refs.stock.value;
+        let unity = this.refs.unity.value;
+        let image  = this.refs.image.value;
+        let provider = this.refs.provider.value;
+        let category = this.refs.category.value;
+
+        if(this.state.act === 0){   //new
+            let data = {
+                name, code, pricePurchase, salePrice, stock, unity, image, provider, category
+            }
+            datas.push(data);
+        }else{                      //update
+            let index = this.state.index;
+            datas[index].name = name;
+            datas[index].code = code;
+            datas[index].pricePurchase = pricePurchase;
+            datas[index].salePrice = salePrice;
+            datas[index].stock = stock;
+            datas[index].unity = unity;
+            datas[index].image = image;
+            datas[index].provider = provider;
+            datas[index].category = category;
+        }
+
+        this.setState({
+            datas: datas,
+            act: 0
+        });
+
+        this.refs.myForm.reset();
+        this.refs.name.focus();
+    }
+
+    fRemove = (i) => {
+        let datas = this.state.datas;
+        datas.splice(i,1);
+        this.setState({
+            datas: datas
+        });
+
+        this.refs.myForm.reset();
+        this.refs.name.focus();
+    }
+
+    fEdit = (i) => {
+        let data = this.state.datas[i];
+        this.refs.name.value = data.name;
+        this.refs.code.value = data.code;
+        this.refs.pricePurchase.value = data.pricePurchase;
+        this.refs.salePrice.value = data.salePrice;
+        this.refs.stock.value = data.stock;
+        this.refs.unity.value = data.unity;
+        this.refs.image.value = data.image;
+        this.refs.provider.value = data.provider;
+        this.refs.category.value = data.category;
+
+        this.setState({
+            act: 1,
+            index: i
+        });
+
+        this.refs.name.focus();
+    }
+
+    render() {
+        let datas = this.state.datas;
+        let datas2 = this.state.datas;
+        let datas3 = this.state.datas;
+        return (
+            <div className="App">
+                <h2>{this.state.title}</h2>
+                <form ref="myForm" className="myForm">
+                    <input type="text" ref="name" placeholder="your name" className="formField" />
+                    <input type="text" ref="code" placeholder="your code" className="formField" />
+                    <input type="text" ref="pricePurchase" placeholder="your price Purchase" className="formField" />
+                    <input type="text" ref="salePrice" placeholder="  sale Price" className="formField" />
+                    <input type="text" ref="stock" placeholder="  stock" className="formField" />
+                    <input type="text" ref="unity" placeholder="  unity" className="formField" />
+                    <input type="text" ref="image" placeholder="  image" className="formField" />
+                    <input type="text" ref="provider" placeholder="  provider" className="formField" />
+                    <input type="text" ref="category" placeholder="  category" className="formField" />
+                    <button onClick={(e)=>this.fSubmit(e)} className="myButton">submit </button>
+                </form>
+                <pre>
+          {datas.map((data, i) =>
+              <li key={i} className="myList">
+                  {i+1}. {data.name}, {data.code}, {data.pricePurchase}, {data.salePrice}
+                  ,{data.stock}, {data.unity}, {data.image}, {data.provider}, {data.category}
+                  <button onClick={()=>this.fRemove(i)} className="myListButton">remove </button>
+                  <button onClick={()=>this.fEdit(i)} className="myListButton">edit </button>
+              </li>
+          )}
+        </pre>
+            </div>
+        );
+    }
 }
 
 export default App;
