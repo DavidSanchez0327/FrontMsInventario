c2d9fd58e486ff181dad5ef48c863a8ba9a760e9
Oscar <oscar.velasquez@sofka.com.co>
2019-09-12 16:43:28 -0500

    signIn

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-    <div v-if="this.loged" id="app" class="container-fluid">
-        <div class="site-info">
-            <h1>Inventario</h1>
+    <div id="app" class="container-fluid">
+        <div v-if="this.loged">
+            <div class="site-info">
+                <h1>Inventario</h1>
+            </div>
+            <nav>
+                <router-link class="btn btn-primary" to="/medicamentos">Medicamentos</router-link>
+                <router-link class="btn btn-primary" to="/medicamentos/guardar">Agregar</router-link>
+                <button class="btn btn-primary" @click="logout">Logout</button>
+            </nav>
         </div>
-        <nav>
-            <router-link class="btn btn-primary" to="/medicamentos">Medicamentos</router-link>
-            <router-link class="btn btn-primary" to="/medicamentos/guardar">Agregar</router-link>
-            <button class="btn btn-primary" @click="logout">Logout</button>
-
-        </nav>
         <br/>
         <router-view/>
     </div>
@@ -18,41 +19,41 @@
 
 <script>
     import firebase from "firebase";
-export default {
-  name: "app",
 
+    export default {
+        name: "app",
 
-    data(){
-      return {
-          loged: true,
-      }
-    },
+        data() {
+            return {
+                loged: true,
+            }
+        },
 
-    methods: {
-      logout() {
-          firebase.auth().signOut().then( res => {
-              this.$router.push('');
-          })
-      }
-    },
+        methods: {
+            logout() {
+                firebase.auth().signOut().then(res => {
+                    this.$router.push('');
+                })
+            }
+        },
 
-    mounted() {
-      this.loged = localStorage.getItem("isLoged");
-    }
-};
+        mounted() {
+            this.loged = localStorage.getItem("isLoged");
+        }
+    };
 </script>
 
 <style>
-.site-info {
-  color: blue;
-  margin-bottom: 20px;
-}
+    .site-info {
+        color: blue;
+        margin-bottom: 20px;
+    }
 
-.btn-primary {
-  margin-right: 5px;
-}
+    .btn-primary {
+        margin-right: 5px;
+    }
 
-.container-fluid {
-  text-align: center;
-}
+    .container-fluid {
+        text-align: center;
+    }
 </style>
